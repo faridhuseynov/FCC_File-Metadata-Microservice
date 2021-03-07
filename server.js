9f5e4e3706c095f09800cf3b2df6a4b1edab6977
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-07 17:18:22 +0400

    form set up for file upload. multer added

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const app = express();
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,4 +10,13 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("avatar"), function (
+  req,
+  res,
+  next
+) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
 });
