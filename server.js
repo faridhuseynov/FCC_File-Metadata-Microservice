1e4f73bdd9b7984411710bf6cee7b7fc567241dc
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-07 17:09:22 +0400

    server.js set up with main page get request

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
