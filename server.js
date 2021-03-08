d3c9395bc97f21d34ee317783ad46e7adaf9026d
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-03-08 19:02:05 +0400

    File uploader set up properly

--- server.js
+++ server.js
@@ -2,21 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const multer = require("multer");
+
 const upload = multer({ dest: "uploads/" });
 const app = express();
+
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/public", express.static(__dirname + "/public"));
+
+const port = process.env.PORT||3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/api/fileanalyse", upload.single("avatar"), function (
+app.post("/api/fileanalyse", upload.single("upfile"), function (
   req,
   res,
   next
 ) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
+  // req.file is the `document` file
+  var document = req.file;
+  res.json({"name":document.originalname,"type":document.mimetype,"size":document.size});
 });
+
+app.listen(port,()=>{
+  console.log("App is running on port: "+port);
+})
